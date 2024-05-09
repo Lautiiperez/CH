7d3b32e164b84a3505c1ca65ee99e85863441569
PEREZ <LAUTPEREZ@sistemas.ad>
2024-05-09 09:52:42 -0300

    1er program python

--- Python/clase1.py
+++ Python/clase1.py
@@ -1,1 +1,5 @@
-print("Hola Mundo!")
+nombre = input("Escribe tu Nombre:")
+apellido = input("Escribe tu Apellido:")
+edad = int(input("Escribe tu Edad:"))
+
+print(nombre +" "+ apellido + " tiene " + str(edad) + " años.")
