13344728c5d474b8c89dc85b16b8ddbc74554968
PEREZ <LAUTPEREZ@sistemas.ad>
2024-05-13 08:51:15 -0300

    ejercicios

--- Python/clase1.py
+++ Python/clase1.py
@@ -1,5 +1,35 @@
-nombre = input("Escribe tu Nombre:")
-apellido = input("Escribe tu Apellido:")
-edad = int(input("Escribe tu Edad:"))
+######################### 1- Mi primer programa
+# nombre = input("Escribe tu Nombre:")
+# apellido = input("Escribe tu Apellido:")
+# edad = int(input("Escribe tu Edad:"))
 
-print(nombre +" "+ apellido + " tiene " + str(edad) + " años.")
+# print(nombre +" "+ apellido + " tiene " + str(edad) + " años.")
+
+
+######################### 2- Desafio String
+# cadena_1 = "versatil"
+# cadena_2 = "Python"
+# cadena_3 = "es un lenguaje"
+# cadena_4 = "de programacion"
+
+# frase = cadena_2 + " "+ cadena_3 + " " + cadena_4 +" "+ cadena_1
+
+# print(frase)
+
+
+######################### 3- Desafio Numeros
+# partidos_ganados = int(input("Cuantos partidos gano?"))
+# partidos_empatados = int(input("Cuantos partidos empato?"))
+# partidos_perdidos = int(input("Cuantos partidos perdio?"))
+
+# total_partidos = partidos_ganados+partidos_empatados+partidos_perdidos
+
+# resultado = ((partidos_ganados * 3) + partidos_empatados + (partidos_perdidos * 0))/(total_partidos)
+
+# if (total_partidos) > 20:
+#     print("La cantidad maxima de partidos debe ser 20")
+# else: 
+#     print("El promedio es: " + str(resultado))
+
+
+######################### 4- Desafio Slicing
