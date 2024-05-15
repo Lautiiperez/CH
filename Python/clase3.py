d1fd1a204be80a101cffea1a259ca0c710db2371
PEREZ <LAUTPEREZ@sistemas.ad>
2024-05-15 10:09:20 -0300

    clase3 - ej2

--- Python/clase3.py
+++ Python/clase3.py
@@ -1,10 +1,16 @@
-######################### 1- Sets
-colores = {"rojo", "blanco", "azul"}
+# ######################### 1- Sets
+# colores = {"rojo", "blanco", "azul"}
 
-colores.update(["violeta", "dorado"])
+# colores.update(["violeta", "dorado"])
 
-colores.discard("blanco")
-colores.remove("dorado")
-colores.discard("celeste")
+# colores.discard("blanco")
+# colores.remove("dorado")
+# colores.discard("celeste")
 
-print(colores)
+# print(colores)
+
+
+######################### 2- Dicts
+copa_fifa = {1990:"alemania", 1994:"brasil", 1998:"francia", 2002:"brasil", 2006:"italia", 2010:"españa", 2014:"alemania", 2018:"francia"}
+
+print(copa_fifa)
