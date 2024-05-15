294f6a6e3ed5b3e6e9993cfb51d6ddf997237831
PEREZ <LAUTPEREZ@sistemas.ad>
2024-05-15 09:42:36 -0300

    clase3

---
+++ Python/clase3.py
@@ -0,0 +1,10 @@
+######################### 1- Sets
+colores = {"rojo", "blanco", "azul"}
+
+colores.update(["violeta", "dorado"])
+
+colores.discard("blanco")
+colores.remove("dorado")
+colores.discard("celeste")
+
+print(colores)
