b3b04812dc2a10d9bc894a5ee946baa2cb9cfc02
lautiiperez <perezlautaro@hotmail.com>
2024-05-20 21:57:24 -0300

    clase3 ej1

---
+++ Python/clase3.py
@@ -0,0 +1,25 @@
+######################### 1- Colecciones 1
+texto = """gordon lanzo su curva&strawberry ha fallado por un pie!
+-grito Joe Castiglione&dos pies -le corrigio
+Troop&strawberry menea la cabeza como disgustado...
+-agrega el comentarista"""
+
+texto = texto.replace('curva&strawberry', 'curva... \n- Strawberry')
+texto = texto.replace("gordon", "Gordon")
+texto = texto.replace("Castiglione&dos", "Castiglione. - Dos")
+texto = texto.replace("Troop&strawberry", "Troop. - Strawberry")
+
+texto2 = list(texto)
+
+texto2[61]=' '
+texto2[85]='\n'
+texto2[97]=''
+texto2[109]=' '
+texto2[116]='\n'
+texto2[164]=' '
+texto2[-1]='a.'
+
+texto = ''.join(texto2)
+
+
+print(texto)
