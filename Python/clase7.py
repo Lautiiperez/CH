03a6ab5352a2a956e4100292f488d84c442a5d85
PEREZ <LAUTPEREZ@sistemas.ad>
2024-05-29 10:12:49 -0300

    ejercicio 1 clase7

--- Python/clase7.py
+++ Python/clase7.py
@@ -4,22 +4,18 @@ num2 = 0
 total = 0
 
 while True:
-    parcial = 0
-    total += total
-    num1 = input("Ingresa un numero: ")
-    if num1 == "exit":
+    parcial = int(num1) + int(num2)
+    total += int(num1) + int(num2)
+
+    num1 = input("Ingresa un numero o exit para terminar: ")
+    if num1.lower() == "exit":
         print(total, " y ", parcial )
         break
-    num2 = input("Ingresa un numero: ")
-    elif num1 == "exit":
+    num2 = input("Ingresa otro numero para sumarlo: ")
+    if num2.lower() == "exit":
         print(total, " y ", parcial )
         break
-    elif type(num2) == int:
-        total = num1 + num2
-        parcial = num1 + num2
-    elif num2 == "exit":
-        print(total, " y ", parcial )
-        break
-    else:
-        print("Dato ingresado no valido")
+    
+    
+    
 
