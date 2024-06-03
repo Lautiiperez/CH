3c7752db64ada9a06db411406a2cd9b7741ce194
lautiiperez <perezlautaro@hotmail.com>
2024-06-03 07:51:57 -0300

    data

---
+++ Data Analitycs/Queries/SQLQuery2.sql
@@ -0,0 +1,7 @@
+/*
+El siguiente codigo crea una base de datos, no se puede crear mas de una con el mismo nombre
+CREATE DATABASE [DATA ANALITYCS 57445]
+
+La puedo eliminar con el siguinte codigo:
+DROP DATABASE [DATA ANALITYCS 57445]
+/*
