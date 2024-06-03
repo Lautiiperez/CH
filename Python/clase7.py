d1a871c5d7c5cf1cfc89171ba06e9b1ac3407de9
lautiiperez <perezlautaro@hotmail.com>
2024-06-03 08:57:40 -0300

    clase7

--- Python/clase7.py
+++ Python/clase7.py
@@ -1,21 +1,37 @@
-######################### 1- Solicitud de Nros al usuario
-num1 = 0
-num2 = 0
-total = 0
+# ######################## 1- Solicitud de Nros al usuario
+# num1 = 0
+# num2 = 0
+# total = 0
 
-while True:
-    parcial = int(num1) + int(num2)
-    total += int(num1) + int(num2)
+# while True:
+#     parcial = int(num1) + int(num2)
+#     total += int(num1) + int(num2)
 
-    num1 = input("Ingresa un numero o exit para terminar: ")
-    if num1.lower() == "exit":
-        print(total, " y ", parcial )
-        break
-    num2 = input("Ingresa otro numero para sumarlo: ")
-    if num2.lower() == "exit":
-        print(total, " y ", parcial )
-        break
+#     num1 = input("Ingresa un numero o exit para terminar: ")
+#     if num1.lower() == "exit":
+#         print(total, " y ", parcial )
+#         break
+#     num2 = input("Ingresa otro numero para sumarlo: ")
+#     if num2.lower() == "exit":
+#         print(total, " y ", parcial )
+#         break
     
-    
+# ######################## 2- Cancion Me Gusta
+
+# cancion = ["Me gustan los aviones", ", me gustas tu", "Me gusta","viajar","la maniana","el viento","soñar","la mar"]
     
 
+# for valor in cancion:
+#     if valor == cancion[0]:
+#         print(cancion[0], cancion[1])
+#     elif valor == cancion[1]:
+#         print("\n",cancion[2], cancion[3],cancion[1])
+#     elif valor == cancion[2]:
+#         print("\n",cancion[2], cancion[4],cancion[1])
+#     elif valor == cancion[3]:
+#         print("\n",cancion[2], cancion[5],cancion[1])
+#     elif valor == cancion[4]:
+#         print("\n",cancion[2], cancion[6],cancion[1])
+#     else:
+#         print("\n",cancion[2], cancion[7],cancion[1])
+#         break
