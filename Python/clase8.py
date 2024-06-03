a1a48e61b1bcad20dfa995f971c01a13195aaf86
PEREZ <LAUTPEREZ@sistemas.ad>
2024-06-03 09:32:27 -0300

    clase8 multiplo

--- Python/clase8.py
+++ Python/clase8.py
@@ -5,14 +5,26 @@
 
 # bienvenido("Carhue")
 
-######################## 2- Media
+# ######################## 2- Media
 
-numeros = [1,5,22,8,6,32,48,6]
+# numeros = [1,5,22,8,6,32,48,6]
 
-def media (lista):
-    suma = 0
-    for valor in lista:
-        suma += valor
-    return print(suma / len(lista))
+# def media (lista):
+#     suma = 0
+#     for valor in lista:
+#         suma += valor
+#     return print(suma / len(lista))
 
-media(numeros)
+# media(numeros)
+
+######################## 3- Multiplo
+num1 = int(input("Ingresa un numero: "))
+num2 = int(input("ingresa otro un numero: "))
+
+def es_multiplo(num1,num2):
+    if (num1%num2) == 0:
+        print(num2,"es multiplo de ", num1)
+    else:
+        print(num2,"NO es multiplo de ", num1)
+
+es_multiplo(num1,num2)
