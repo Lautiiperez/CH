d0cd66333b9f22ad335e98c8a87f699d49708eef
lautiiperez <perezlautaro@hotmail.com>
2024-06-12 20:50:45 -0300

    preentrega

--- Python/primerEntrega.py
+++ Python/primerEntrega.py
@@ -3,8 +3,8 @@ usuarios = {}
 
 # Función que valida la contraseña y usuario
 def validar_usuario_y_contrasena(usuario, contrasena):
-    if len(usuario) < 4 or len(usuario) > 10:
-        return False, "El nombre de usuario debe tener entre 4 y 10 caracteres."
+    if len(usuario) < 4 or len(usuario) > 12:
+        return False, "El nombre de usuario debe tener entre 4 y 12 caracteres."
     if len(contrasena) < 6:
         return False, "La contraseña debe ser mayor de 6 caracteres."
     if len(contrasena) > 12:
@@ -36,7 +36,8 @@ def iniciar_sesion(usuario, contrasena=None):
         else:
             print("Contraseña incorrecta.")
     else:
-        continuar = input(f"El usuario '{usuario}' no existe. ¿Desea registrarte? (s/n): ").lower()
+        print("El usuario no existe.")
+        continuar = input("¿Desea registrarte? (s/n): ").lower()
         if continuar == 's':
             usuario_registro = input("Ingrese el nombre de usuario para el registro: ")
             contrasena_registro = input("Ingrese la contraseña para el registro: ")
@@ -58,22 +59,61 @@ def leer_usuarios():
             for linea in archivo.readlines():
                 usuario, contrasena = linea.strip().split(':')
                 usuarios[usuario] = contrasena
-        print("Usuarios cargados desde el archivo.")
+        print("Cargando usuarios del archivo...")
+        
     except FileNotFoundError:
-        print("No se encontró el archivo de usuarios. Creando uno nuevo...")
+        print("No se encontró el archivo de usuarios.\nCreando un registro nuevo...")
 
-# Función que mustra los usuarios ya registrados
+# Función que muestra los usuarios ya registrados
 def mostrar_usuarios():
     print("Usuarios registrados:")
     for usuario in usuarios.keys():
         print(usuario)
 
+def cambiar_contrasena():
+    usuario = input("Ingrese el nombre de usuario: ")
+    if usuario not in usuarios:
+        continuar = input(f"El usuario '{usuario}' no existe. ¿Desea registrarte? (s/n): ").lower()
+        if continuar == 's':
+            usuario_registro = input("Ingrese el nombre de usuario para el registro: ")
+            contrasena_registro = input("Ingrese la contraseña para el registro: ")
+            valido, mensaje_error = validar_usuario_y_contrasena(usuario_registro, contrasena_registro)
+            if not valido:
+                print(mensaje_error)
+                return
+            
+            usuarios[usuario_registro] = contrasena_registro
+            print("Usuario registrado exitosamente.")
+        else:
+            print("No se realizó ninguna acción.")
+            return
+    
+    # Verificar si el usuario ya tiene una contraseña establecida
+    if usuarios[usuario] is None:
+        print("El usuario no ha establecido una contraseña aún. Por favor, establezca una contraseña primero.")
+        return
+    
+    contrasena_actual = input("Ingrese la contraseña actual: ")
+    if usuarios[usuario]!= contrasena_actual:
+        print("La contraseña actual es incorrecta.")
+        return
+    
+    nueva_contrasena = input("Ingrese la nueva contraseña: ")
+    valido, mensaje_error = validar_usuario_y_contrasena(usuario, nueva_contrasena)
+    if not valido:
+        print(mensaje_error)
+        return
+    
+    usuarios[usuario] = nueva_contrasena
+    print(f"La contraseña de {usuario} ha sido cambiada exitosamente.")
+    guardar_usuarios()
+
 # Función principal (Menu)
 def main():
     while True:
         try:
-            print("\n1. Ver usuarios registrados\n2. Registrar usuario\n3. Iniciar sesión\n4. Salir")
-            opcion = int(input("Seleccione una opción: "))  # Intentamos convertir la entrada a entero
+            print("\n1. Ver usuarios registrados\n2. Registrar usuario\n3. Iniciar sesión\n4. Cambiar contraseña\n5. Salir")
+            opcion = int(input("Seleccione una opción: "))
             
             if opcion == 1:
                 leer_usuarios()
@@ -86,13 +126,15 @@ def main():
                 usuario = input("Ingrese el nombre de usuario: ")
                 iniciar_sesion(usuario)
             elif opcion == 4:
+                cambiar_contrasena()
+            elif opcion == 5:
                 print("Saliendo...")
                 break
             else:
-                print("Opción inválida.")
+                print("Opción inválida. Ingrese una opción del 1 al 5.")
                 
         except ValueError:
-            print("Por favor, ingrese un número válido.")  # Capturamos el ValueError y mostramos un mensaje de error
+            print("Por favor, ingrese un número válido.")
 
 if __name__ == "__main__":
     main()
